7a7c39f9e384615d778c109af9f04310d9aa94ca
Aaron Jacobs <aaron.jacobs@rstudio.com>
2021-12-23 19:23:07 -0500

    Add a "decrypt" command, mostly for roundtrip testing.
    Signed-off-by: Aaron Jacobs <aaron.jacobs@rstudio.com>

---
+++ cmd/decrypt.go
@@ -0,0 +1,61 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/rstudio/rskey/crypt"
+)
+
+// decryptCmd represents the decrypt command
+var decryptCmd = &cobra.Command{
+	Use:   "decrypt",
+	Short: "Decrypt previously-decrypted data",
+	Long: `Use a RStudio Connect/Package Manager key to decrypt data passed on
+standard output.`,
+	RunE: runDecrypt,
+}
+
+func runDecrypt(cmd *cobra.Command, args []string) error {
+	keyfile := cmd.Flag("keyfile").Value.String()
+	if keyfile == "" {
+		return fmt.Errorf("keyfile is missing but must be provided")
+	}
+	f, err := os.Open(keyfile)
+	if err != nil {
+		return err
+	}
+	key, err := crypt.NewKeyFromReader(f)
+	if err != nil {
+		return err
+	}
+	// Check if there's actually data in standard input.
+	info, err := os.Stdin.Stat()
+	if err != nil {
+		return err
+	}
+	if info.Mode()&os.ModeNamedPipe == 0 {
+		return fmt.Errorf("No input")
+	}
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		cipher, err := key.Decrypt(scanner.Text())
+		if err != nil {
+			return err
+		}
+		fmt.Fprintf(cmd.OutOrStdout(), "%s\n", cipher)
+	}
+	return nil
+}
+
+func init() {
+	rootCmd.AddCommand(decryptCmd)
+	decryptCmd.Flags().StringP("keyfile", "f", "", "Use the given key file")
+}
